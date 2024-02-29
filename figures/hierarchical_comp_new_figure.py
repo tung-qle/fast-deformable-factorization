c6e322b7fe0a0a54913fa21ce249571eaa5996e7
ZHENG Leon <leon.zheng@inria.fr>
2024-02-29 11:31:32 +0100

    test hierarchical comp new

--- figures/hierarchical_comp_new_figure.py
+++ figures/hierarchical_comp_new_figure.py
@@ -14,6 +14,7 @@ def parse_arguments():
     args.add_argument("--hierarchical-order", type=str, default="balanced", choices=["left-to-right", "balanced"])
     args.add_argument("--matrix-size", type=int, nargs="+", default=[128, 256, 512, 1024, 2048, 4096, 8192])
     args.add_argument("--seed-target-matrix", type=int, nargs="+", default=list(range(10)))
+    args.add_argument("--graph", choices=["time", "error"])
     return args.parse_args()
 
 
@@ -31,33 +32,70 @@ if __name__ == '__main__':
 
     print(df)
 
-    # Time comparison
-    time_mean = df[["orthonormalize", "matrix-size", "time"]].groupby(["matrix-size", "orthonormalize"]).mean()
-    time_mean = time_mean.rename(columns={"time": "time_mean"})
-    time_std = df[["orthonormalize", "matrix-size", "time"]].groupby(["matrix-size", "orthonormalize"]).std()
-    time_std = time_std.rename(columns={"time": "time_std"})
-    time_df = time_mean.join(time_std)
-    print(time_df)
+    if args.graph == "time":
+        # Time comparison
+        time_mean = df[["orthonormalize", "matrix-size", "time"]].groupby(["matrix-size", "orthonormalize"]).mean()
+        time_mean = time_mean.rename(columns={"time": "time_mean"})
+        time_std = df[["orthonormalize", "matrix-size", "time"]].groupby(["matrix-size", "orthonormalize"]).std()
+        time_std = time_std.rename(columns={"time": "time_std"})
+        time_df = time_mean.join(time_std)
+        print(time_df)
 
-    fig, ax = plt.subplots()
-    # plot time vs matrix size with error bars following std
-    for orthonormalize in [True, False]:
-        time_df_local = time_df.loc[(slice(None), orthonormalize), :]
-        matrix_sizes = time_df_local.index.levels[0]
-        print(time_df_local)
-        print(matrix_sizes)
-        ax.errorbar(matrix_sizes, time_df_local["time_mean"], yerr=time_df_local["time_std"], marker="x", label=f"orthonormalize={orthonormalize}")
+        fig, ax = plt.subplots()
+        # plot time vs matrix size with error bars following std
+        for orthonormalize in [True, False]:
+            time_df_local = time_df.loc[(slice(None), orthonormalize), :]
+            matrix_sizes = time_df_local.index.levels[0]
+            print(time_df_local)
+            print(matrix_sizes)
+            ax.errorbar(matrix_sizes, time_df_local["time_mean"], yerr=time_df_local["time_std"], marker="x", label=f"orthonormalize={orthonormalize}")
 
-    ax.set_yscale("log")
-    # set log scale in base 2
-    ax.set_xscale("log", base=2)
-    ax.set_xlabel("Matrix size")
-    ax.set_ylabel("Time (s)")
-    ax.grid()
-    ax.legend()
+        ax.set_yscale("log")
+        # set log scale in base 2
+        ax.set_xscale("log", base=2)
+        ax.set_xlabel("Matrix size")
+        ax.set_ylabel("Time (s)")
+        ax.grid()
+        ax.legend()
 
-    params = f"n-factors={args.n_factors}-rank={args.rank}-noise={args.noise}-hierarchical-order={args.hierarchical_order}"
-    ax.set_title(params)
+        params = f"n-factors={args.n_factors}-rank={args.rank}-noise={args.noise}-hierarchical-order={args.hierarchical_order}"
+        ax.set_title(params)
 
-    plt.savefig(args.results_dir / f"time_vs_matrix_size_{params}.pdf")
-    plt.show()
+        plt.savefig(args.results_dir / f"time_vs_matrix_size_{params}.pdf")
+        plt.show()
+
+    elif args.graph == "error":
+        # Error comparison
+        error_mean = df[["orthonormalize", "matrix-size", "error-relative"]].groupby(["matrix-size", "orthonormalize"]).mean()
+        error_mean = error_mean.rename(columns={"error-relative": "error_mean"})
+        error_std = df[["orthonormalize", "matrix-size", "error-relative"]].groupby(["matrix-size", "orthonormalize"]).std()
+        error_std = error_std.rename(columns={"error-relative": "error_std"})
+        error_df = error_mean.join(error_std)
+
+        fig, ax = plt.subplots()
+        # plot time vs matrix size with error bars following std
+        for orthonormalize in [True, False]:
+            error_df_local = error_df.loc[(slice(None), orthonormalize), :]
+            matrix_sizes = error_df_local.index.levels[0]
+            ax.errorbar(matrix_sizes, error_df_local["error_mean"], yerr=error_df_local["error_std"], marker="x", label=f"orthonormalize={orthonormalize}")
+
+        # show as horizontal bar the noise level
+        noise_level = args.noise
+        ax.axhline(y=noise_level, color='r', linestyle='--', label="noise level")
+
+        # ax.set_yscale("log")
+        # set log scale in base 2
+        ax.set_xscale("log", base=2)
+        ax.set_xlabel("Matrix size")
+        ax.set_ylabel("Relative error")
+        ax.grid()
+        ax.legend()
+
+        params = f"n-factors={args.n_factors}-rank={args.rank}-noise={args.noise}-hierarchical-order={args.hierarchical_order}"
+        ax.set_title(params)
+
+        plt.savefig(args.results_dir / f"error_vs_matrix_size_{params}.pdf")
+        plt.show()
+
+    else:
+        raise NotImplementedError
--- figures/plot_time_figures.sh
+++ figures/plot_time_figures.sh
@@ -2,11 +2,15 @@ for rank in 1 2 4
 do
   for hierarchical_order in left-to-right balanced
   do
-    python hierarchical_comp_new_figure.py \
-      --results-dir results/hierarchical_comp_new/anchiale1 \
-      --n-factors 4 \
-      --noise 0.1 \
-      --rank $rank \
-      --hierarchical-order $hierarchical_order
+    for graph in time error
+    do
+      python figures/hierarchical_comp_new_figure.py \
+        --results-dir results/hierarchical_comp_new/anchiale1 \
+        --n-factors 4 \
+        --noise 0.1 \
+        --rank $rank \
+        --hierarchical-order $hierarchical_order \
+        --graph $graph
+    done
   done
 done
